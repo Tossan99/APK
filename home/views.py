d10b5c881c71c73b96796260350fb00b688b34b0
Erik Guldbrand <130132826+Tossan99@users.noreply.github.com>
2024-02-29 11:08:27 +0000

    Add

--- home/views.py
+++ home/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_page(request):
-    return HttpResponse("Hello, Blog!")
+    """
+    A view for the home page
+    """
+    return render(request, 'beverages/index.html')
